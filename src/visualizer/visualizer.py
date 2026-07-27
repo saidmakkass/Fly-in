e5adacef5fab42adc09e61aa76b68870da0e3a72
Said Makkass <saidmakkass1@gmail.com>
2026-07-27 21:59:54 +0100

    refactor(visualizer): Add graph view

--- src/visualizer/visualizer.py
+++ src/visualizer/visualizer.py
@@ -8,9 +8,20 @@ class Visualizer:
     def __init__(self, graph: Graph):
         self.graph = graph
         self.window = VisualizerWindow()
+        self.graph_view = GraphView(self.window)
 
     def run(self):
-        self.window.run()
+        self.window.run(self.graph_view)
+
+class GraphView(arcade.View):
+    def __init__(self, window: arcade.Window):
+        super().__init__(window)
+
+    def on_draw(self):
+        pass
+
+    def on_update(self, delta_time):
+        pass
 
 class VisualizerWindow(arcade.Window):
     def __init__(self):
