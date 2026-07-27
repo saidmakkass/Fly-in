c58133ec566668805991093d15a9530cba13b664
Said Makkass <saidmakkass1@gmail.com>
2026-07-27 20:40:46 +0100

    refactor(visualizer): Add converter for Map objects

--- src/visualizer/converter.py
+++ src/visualizer/converter.py
@@ -1,10 +1,33 @@
-from typing import Tuple
+from typing import Tuple, List
 
-from ..map_parser import Zone, Connection
+from ..map_parser import Map, Zone, Connection
 
 
 class Converter:
     """Utility class to convert Fly-in related objects to tuples"""
+
+    @staticmethod
+    def convert_map(
+        map: Map,
+    ) -> Tuple[
+        List[Tuple[str, int, int, str, str, int]],
+        List[Tuple[int, int, int, int, int]],
+        int,
+    ]:
+        """
+        Convert a Map object to a tuple.
+
+        Args:
+            map (Map): The Map object
+
+        Returns:
+            map (tuple): The map as a tuple
+        """
+        return (
+            [Converter.convert_zone(z) for z in map.zones],
+            [Converter.convert_connection(c) for c in map.connections],
+            map.nb_drones
+        )
 
     @staticmethod
     def convert_zone(zone: Zone) -> Tuple[str, int, int, str, str, int]:
