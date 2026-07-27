fd6ef7a5e26d121bf79435d153db25e21f6d99d2
Said Makkass <saidmakkass1@gmail.com>
2026-07-27 22:05:31 +0100

    refactor(visualizer): Add background loading and rendering

--- src/visualizer/visualizer.py
+++ src/visualizer/visualizer.py
@@ -1,8 +1,10 @@
 import arcade
+from importlib.resources import files
 
 from .converter import Graph
 
 WINDOW_TITLE = "Fly-in"
+
 
 class Visualizer:
     def __init__(self, graph: Graph):
@@ -13,15 +15,31 @@ class Visualizer:
     def run(self):
         self.window.run(self.graph_view)
 
+
 class GraphView(arcade.View):
     def __init__(self, window: arcade.Window):
         super().__init__(window)
+        self.__load_assets()
+
+    def __load_assets(self):
+        assets_dir = files(__package__) / "assets"
+
+        self.background_image = arcade.load_texture(
+            assets_dir / "background.jpg"
+        )
+
+    def __draw_background(self):
+        arcade.draw_texture_rect(
+            self.background_image,
+            arcade.rect.Viewport(0, 0, self.width, self.height),
+        )
 
     def on_draw(self):
-        pass
+        self.__draw_background()
 
     def on_update(self, delta_time):
         pass
+
 
 class VisualizerWindow(arcade.Window):
     def __init__(self):
