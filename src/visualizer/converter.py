e202114cb4622dc4b59bdf2dbf1b97445b28044e
Said Makkass <saidmakkass1@gmail.com>
2026-07-27 20:55:29 +0100

    refactor(visualizer): Add aliases for converted data types

--- src/visualizer/converter.py
+++ src/visualizer/converter.py
@@ -2,43 +2,44 @@ from typing import Tuple, List
 
 from ..map_parser import Map, Zone, Connection
 
+Node = Tuple[str, int, int, str, str, int]
+Edge = Tuple[int, int, int, int, int]
+Graph = Tuple[List[Node], List[Edge], int]
+
 
 class Converter:
-    """Utility class to convert Fly-in related objects to tuples"""
+    """
+    Utility class to convert Fly-in related objects
+    to objects used by Visualizer
+    """
 
     @staticmethod
-    def convert_map(
-        map: Map,
-    ) -> Tuple[
-        List[Tuple[str, int, int, str, str, int]],
-        List[Tuple[int, int, int, int, int]],
-        int,
-    ]:
+    def convert_map(map: Map) -> Graph:
         """
-        Convert a Map object to a tuple.
+        Convert a Map object to a Graph.
 
         Args:
             map (Map): The Map object
 
         Returns:
-            map (tuple): The map as a tuple
+            map (Graph): The map as a Graph
         """
         return (
             [Converter.convert_zone(z) for z in map.zones],
             [Converter.convert_connection(c) for c in map.connections],
-            map.nb_drones
+            map.nb_drones,
         )
 
     @staticmethod
-    def convert_zone(zone: Zone) -> Tuple[str, int, int, str, str, int]:
+    def convert_zone(zone: Zone) -> Node:
         """
-        Convert a Zone object to a tuple.
+        Convert a Zone object to a Node.
 
         Args:
             zone (Zone): The Zone object.
 
         Returns:
-            zone (tuple): The zone as a tuple.
+            zone (Node): The zone as a Node.
         """
         return (
             zone.name,
@@ -50,17 +51,15 @@ class Converter:
         )
 
     @staticmethod
-    def convert_connection(
-        connection: Connection,
-    ) -> Tuple[int, int, int, int, int]:
+    def convert_connection(connection: Connection) -> Edge:
         """
-        Convert a Connection object to a tuple.
+        Convert a Connection object to a Edge.
 
         Args:
             connection (Connection): The Connection object.
 
         Returns:
-            connection (tuple): The connection as a tuple.
+            connection (Edge): The connection as a Edge.
         """
         return (
             connection.zone_a.x,
